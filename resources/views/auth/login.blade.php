be68804bcafc0380349b5087a145a407cb32a4b2
wojst <wojtek4531@gmail.com>
2023-05-28 21:28:04 +0200

    navbar 4 login panel added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     </head>
     <body>
 
-        @include('shared.navbar')
+        @include('shared.navbar4loginpanel')
 
         <section class="vh-100">
             <div class="row d-flex align-items-center justify-content-center h-100">
