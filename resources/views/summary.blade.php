6e4b8aadd4249b7250997d332892702f581b43aa
wojst <wojtek4531@gmail.com>
2023-05-30 20:57:48 +0200

    added button for homepage comeback

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -7,41 +7,45 @@
         <div class="container h-100 py-5">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-10">
-                    <div class="d-flex justify-content-between align-items-center mb-4">
-                        <h3 class="fw-normal mb-0 text-black">Podsumowanie</h3>
+                    <div class="d-flex justify-content-center align-items-center mb-4">
+                        <h3 class="fw-normal mb-0 text-center">Podsumowanie</h3>
                     </div>
 
                     <div class="card rounded-3 mb-4">
                         @if($movie)
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <img src="{{ asset($movie->img_path) }}" alt="{{ $movie->title }}" class="rounded-3 img-fluid">
-                                        </div>
-                                        <div class="col-md-9">
-                                            <h5 class="card-title">{{ $movie->title }}</h5>
-                                            <p class="card-text">Gatunek: {{ $movie->category->category_name }}</p>
-                                            <p class="movie-price">Czas wypożyczenia:
-                                                <select name="rental_duration" id="rental_duration">
-                                                    <option value="24">24 godziny</option>
-                                                    <option value="48">48 godzin</option>
-                                                </select>
-                                            </p>
-                                            <p class="movie-price">Cena: <span id="movie-price"></span> zł</p>
-                                        </div>
+
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="{{ asset($movie->img_path) }}" alt="{{ $movie->title }}" class="rounded-3 img-fluid">
+                                    </div>
+                                    <div class="col-md-9">
+                                        <h5 class="card-title">{{ $movie->title }}</h5>
+                                        <p class="card-text">Gatunek: {{ $movie->category->category_name }}</p>
+                                        <p class="movie-price">Czas wypożyczenia:
+                                            <select name="rental_duration" id="rental_duration">
+                                                <option value="24">24 godziny</option>
+                                                <option value="48">48 godzin</option>
+                                            </select>
+                                        </p>
+                                        <p class="movie-price">Cena: <span id="movie-price"></span> zł</p>
                                     </div>
                                 </div>
                             </div>
+
                         @else
                             <p class="empty-cart">Koszyk jest pusty.</p>
                         @endif
                     </div>
 
-                    <div class="card">
+                    <div class="card text-center">
                         <div class="card-body align-items-center">
                             <a href="{{ route('payment') }}" class="btn btn-warning btn-block btn-lg">Przejdź do płatności</a>
                         </div>
+                    </div>
+
+                    <div class="text-center mt-4">
+                        <a href="{{ route('homepage') }}" class="btn btn-primary">Powrót do strony głównej</a>
                     </div>
                 </div>
             </div>
@@ -56,6 +60,9 @@
                 document.getElementById('movie-price').textContent = price;
             });
         </script>
+
+
+
 
 
 
