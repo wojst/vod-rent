1fb824f838fdab059306f1f9353c352fdf733cab
wojst <wojtek4531@gmail.com>
2023-06-18 16:49:11 +0200

    better searching movies

--- resources/views/ourmovies.blade.php
+++ resources/views/ourmovies.blade.php
@@ -17,7 +17,7 @@
 
             <div class="d-flex justify-content-center">
                 <form class="d-flex justify-content-center" role="search" action="{{ route('searchMovies') }}" method="GET">
-                    <input class="form-control me-2" type="search" name="search" placeholder="Wpisz tytuł, aby wyszukać..." aria-label="Search">
+                    <input class="form-control me-2" type="search" name="search" placeholder="Wpisz tytuł lub aktora, aby wyszukać..." aria-label="Search">
 
                     <select class="form-select" name="category">
                         <option value="">Wszystkie kategorie</option>
@@ -34,57 +34,40 @@
 
             {{-- displaying container --}}
 
-            <div class="row align-items-stretch">
-
-                @foreach($movies as $movie)
-                    <div class="col-md-6 col-lg-6 col-xl-3 d-flex mb-4">
-                        <div class="card flex-fill">
-                            <img src="{{ asset($movie->img_path) }}" class="card-img-top" alt="img">
-                            <div class="card-body d-flex flex-column">
-                                <h4 class="card-title">{{ $movie->title }}</h4>
-                                <p class="card-text h-100">{{ $movie->description }}</p>
-                                <p class="card-text">Obsada:
-                                    @php
-                                        $actors = $movie->actors->pluck('actor_name')->toArray();
-                                        echo implode(', ', $actors);
-                                    @endphp
-                                </p>
-                                <p class="card-text">Reżyser: {{ $movie->director }}</p>
-                                <p class="card-text">Gatunek: {{ optional($movie->category)->category_name ?? '' }}</p>
-                                <p class="card-text">{{ $movie->release_year }}</p>
-                                <h6 class="card-text">Cena: {{ $movie->price }} zł</h6>
-                                @if(Auth::check())
-                                    <a href="{{ route('summary', ['movie_id' => $movie->movie_id]) }}" class="btn btn-primary">Wypożycz</a>
-                                @else
-                                    <a href="{{ route('login') }}" class="btn btn-primary">Zaloguj się, aby wypożyczyć</a>
-                                @endif
+            @if(count($movies) > 0)
+                <div class="row align-items-stretch">
+                    @foreach($movies as $movie)
+                        <div class="col-md-6 col-lg-6 col-xl-3 d-flex mb-4">
+                            <div class="card flex-fill">
+                                <img src="{{ asset($movie->img_path) }}" class="card-img-top" alt="img">
+                                <div class="card-body d-flex flex-column">
+                                    <h4 class="card-title">{{ $movie->title }}</h4>
+                                    <p class="card-text h-100">{{ $movie->description }}</p>
+                                    <p class="card-text">Obsada:
+                                        @php
+                                            $actors = $movie->actors->pluck('actor_name')->toArray();
+                                            echo implode(', ', $actors);
+                                        @endphp
+                                    </p>
+                                    <p class="card-text">Reżyser: {{ $movie->director }}</p>
+                                    <p class="card-text">Gatunek: {{ optional($movie->category)->category_name ?? '' }}</p>
+                                    <p class="card-text">{{ $movie->release_year }}</p>
+                                    <h6 class="card-text">Cena: {{ $movie->price }} zł</h6>
+                                    @if(Auth::check())
+                                        <a href="{{ route('summary', ['movie_id' => $movie->movie_id]) }}" class="btn btn-primary">Wypożycz</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="btn btn-primary">Zaloguj się, aby wypożyczyć</a>
+                                    @endif
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
-
-            {{-- pagination --}}
-
-            {{-- <div class="d-flex justify-content-center mt-4">
-                <ul class="pagination">
-                    <li class="page-item {{ $movies->previousPageUrl() ? '' : 'disabled' }}">
-                        <a class="page-link" href="{{ $movies->previousPageUrl() }}" aria-label="Poprzednia">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-                    @foreach ($movies->getUrlRange(1, $movies->lastPage()) as $page => $url)
-                        <li class="page-item {{ $page == $movies->currentPage() ? 'active' : '' }}">
-                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
-                        </li>
                     @endforeach
-                    <li class="page-item {{ $movies->nextPageUrl() ? '' : 'disabled' }}">
-                        <a class="page-link" href="{{ $movies->nextPageUrl() }}" aria-label="Następna">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
-                </ul>
-            </div> --}}
+                </div>
+            @else
+                <div class="d-flex justify-content-center align-items-center" style="height: 200px;">
+                    <p style="font-size: 24px;">Brak filmów do wyświetlenia</p>
+                </div>
+            @endif
 
         </div>
 
