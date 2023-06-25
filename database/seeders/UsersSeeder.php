346cc227fcb33ffca15ce15f798b3c1fd8192d0d
wojst <wojtek4531@gmail.com>
2023-06-25 19:52:00 +0200

    add a user to start seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -15,11 +15,23 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
+        // Dodanie użytkownika z rolą administratora
         DB::table('users')->insert([
             'admin_role' => true,
             'name' => 'admin0',
             'email' => 'admin0@example.com',
             'password' => Hash::make('admin123'),
+            'orders_count' => 0,
+            'loyalty_card' => false,
+            'id_fav_category' => null,
+        ]);
+
+        // Dodanie nowego użytkownika, który nie jest administratorem
+        DB::table('users')->insert([
+            'admin_role' => false,
+            'name' => 'user1',
+            'email' => 'user1@example.com',
+            'password' => Hash::make('user123'),
             'orders_count' => 0,
             'loyalty_card' => false,
             'id_fav_category' => null,
